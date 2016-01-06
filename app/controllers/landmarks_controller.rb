96f4063b5b0b6efeb88c34d40486b162faa6d950
Justin Thomas <jthomas0209@gmail.com>
2016-01-06 09:32:45 -0500

    erb :new complete

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,16 @@
 class LandmarksController < ApplicationController
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+
+  post "/landmarks" do
+    @new_landmark = Landmark.create(name: params["landmark"]["name"], year_completed: params["landmark"]["year_completed"])
+    redirect to "/landmarks/:id"
+  end
+
+  get "/landmarks" do
+    @landmarks = Landmarks.all
+    erb :"landmarks/index"
+  end
 
 end
